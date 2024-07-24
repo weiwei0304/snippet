c58aa391af1c1b796b7d7990dc8c115901fa7c06
weiwei <weiwendy0430@gmail.com>
2024-07-24 20:04:39 +0800

    course 30 finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
 import Image from 'next/image';
+import { db } from '@/db';
+import Link from 'next/link';
 
-export default function Home() {
-  return <div>HomePage</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SnippetLoadingPage() {
+  return <div>Loading...</div>;
+}
