eb1be743f33a5f6997baa8c24dc2690a0c24e6c1
weiwei <weiwendy0430@gmail.com>
2024-07-25 22:48:20 +0800

    course 33 6:58

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+
+interface SnippetEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const id = parseInt(props.params.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>Editing snippet with title {snippet.title}</div>;
+}
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { db } from '@/db';
 import { notFound } from 'next/navigation';
 
+import Link from 'next/link';
+
 interface SnippetShowPageProps {
   params: {
     id: string;
@@ -17,5 +19,24 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
